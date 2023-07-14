df636d7c59bd0a6bbf0d728be8dbeafb6d80f9ef
Coleynick <ezechidinma14@gmail.com>
2023-07-13 19:42:47 -0700

    Update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 def main():
     a = 10
     b = 5
-    calculator = __import__('calculator_1')
+    import calculator_1
     print("{:d} + {:d} = {:d}".format(a, b, calculator.add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, calculator.sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, calculator.mul(a, b)))
