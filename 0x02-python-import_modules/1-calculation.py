93cef7cdcd5a6390a1eaf5089879df9cfa412d86
Coleynick <ezechidinma14@gmail.com>
2023-07-13 19:21:37 -0700

    Make executable file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 a = 10
 b = 5
-calculator = __import__('calculator_1')
+add_result = calculator_1.add(a, b)
+sub_result = calculator_1.sub(a, b)
+mul_result = calculator_1.mul(a, b)
+div_result = calculator_1.div(a, b)
 print("{:d} + {:d} = {:d}".format(a, b, calculator.add(a, b)))
 print("{:d} - {:d} = {:d}".format(a, b, calculator.sub(a, b)))
 print("{:d} * {:d} = {:d}".format(a, b, calculator.mul(a, b)))
