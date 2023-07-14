6d5c1b468187e8932c662b8b949eda9e4d05582a
Coleynick <ezechidinma14@gmail.com>
2023-07-13 19:33:41 -0700

    Update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-a = 10
-b = 5
-add_result = calculator_1.add(a, b)
-sub_result = calculator_1.sub(a, b)
-mul_result = calculator_1.mul(a, b)
-div_result = calculator_1.div(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, calculator.add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, calculator.sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, calculator.mul(a, b)))
-print("{:d} / {:d} = {:.0f}".format(a, b, calculator.div(a, b)))
+def main():
+    a = 10
+    b = 5
+    calculator = __import__('calculator_1')
+    print("{:d} + {:d} = {:d}".format(a, b, calculator.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calculator.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calculator.mul(a, b)))
+    print("{:d} / {:d} = {:.2f}".format(a, b, calculator.div(a, b)))
+
+
+if __name__ == "__main__":
+    main()
