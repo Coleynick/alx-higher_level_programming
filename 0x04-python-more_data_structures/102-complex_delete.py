a3c5ae496f015c7d8675edf5a3b108bc4e6cd670
Coleynick <ezechidinma14@gmail.com>
2023-07-19 18:51:57 -0700

    Delete keys with a specific value in a dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+from typing import Dict
+from print_sorted_dictionary import print_sorted_dictionary
+
 def complex_delete(a_dictionary, value):
     keys_to_delete = [key for key, val in a_dictionary.items() if val == value]
     for key in keys_to_delete:
         a_dictionary.pop(key)
     return a_dictionary
-
-def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print("{}: {}".format(key, a_dictionary[key]))
