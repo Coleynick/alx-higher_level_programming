414c21c6cff6292b3752ffbbd145490d6002f7ce
Coleynick <ezechidinma14@gmail.com>
2023-07-31 20:01:48 -0700

    Divide element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,4 +15,6 @@ def list_division(my_list_1, my_list_2, list_length):
             total.append(0)
         except IndexError:
             print("out of range")
+        finally:
+            pass
     return (total)
