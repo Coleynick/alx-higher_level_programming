378f65f1c16263d6f12995034a4344b07d10b0a8
Coleynick <ezechidinma14@gmail.com>
2023-07-31 19:59:21 -0700

    Divide element by element 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    total = []
+    for y in range(list_length):
+        try:
+            a = float(my_list_1[y]) if y < len(my_list_1) else 0.0
+            b = float(my_list_2[y]) if y < len(my_list_2) else 1.0
+            total_division = a / b
+            total.append(total_division)
+        except ZeroDivisionError:
+            print("division by 0")
+            total.append(0)
+        except (ValueError, TypeError):
+            print("wrong type")
+            total.append(0)
+        except IndexError:
+            print("out of range")
+    return (total)
