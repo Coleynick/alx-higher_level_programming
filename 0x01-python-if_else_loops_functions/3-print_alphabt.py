9a30e36d75012534de0580d4191463c1c6aa327a
Coleynick <ezechidinma14@gmail.com>
2023-08-05 07:36:28 -0700

    Print the ASCII alphabet in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for ascii_val in range(ord('a'), ord('z') + 1):
     if ascii_val == ord('q') or ascii_val == ord('e'):
         continue
-    print(chr(ascii_val), end='')
+    print("{}".format(chr(ascii_val)), end='')
