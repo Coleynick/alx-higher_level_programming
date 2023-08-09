de7204757246f1c6f4c96e997da6fa19ba172ac5
Coleynick <ezechidinma14@gmail.com>
2023-08-08 19:00:50 -0700

    Multiply 2 matrices by using the module NumPy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Multiplies 2 matrices by NumPy.
+
+    Args:
+        m_a (list of lists): Matrix (No. 1).
+        m_b (list of lists): Matrix (No. 2).
+
+    Returns:
+        list of lists: Matrix multiplication result.
+    """
+    return np.matmul(m_a, m_b)
