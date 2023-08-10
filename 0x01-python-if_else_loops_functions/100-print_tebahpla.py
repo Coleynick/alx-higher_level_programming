3b3c67055eb0bf86eb283f4bb1c4727a8c4f2620
Coleynick <ezechidinma14@gmail.com>
2023-08-09 20:11:28 -0700

    Alternate lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}{:c}".format(i, i - 32), end='')
+for a in range(ord("z"), ord("a")-1, -1):
+    if a % 2 == 1:
+        y = chr(a - ord("a") + ord("A"))
+    else:
+        y = chr(a)
+    print("{}".format(str(y)), end='')
