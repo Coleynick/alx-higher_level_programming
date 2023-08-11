69191b02dca37a350afb77a1461ca63f9683da9a
Coleynick <ezechidinma14@gmail.com>
2023-08-10 19:47:26 -0700

    hide

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,7 +5,7 @@ import hidden_4 as p
 def main():
     name = dir(p)
     for n in name:
-        if name[0] == "__":
+        if n[0] == "__":
             return
         else:
             print(n)
