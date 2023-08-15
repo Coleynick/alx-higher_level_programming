ab53d4d40ae1b213975063ca543bd4bd0906b15c
Coleynick <ezechidinma14@gmail.com>
2023-08-14 17:17:21 -0700

    Write a class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+class MyInt(int):
+    """
+    An integer class.
+    """
+
+    def __eq__(self, other):
+        """
+        Check if not equal.
+
+        Args:
+            other: compare with.
+
+        Returns:
+            bool: True != equal, False otherwise.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Check if two MyInt are equal.
+
+        Args:
+            other: compare with.
+
+        Returns:
+            bool: True if equal, False otherwise.
+        """
+        return super().__eq__(other)
