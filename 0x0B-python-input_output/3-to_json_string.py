83e681bf1f72607c7b0b106d6b66b4930b77da15
Coleynick <ezechidinma14@gmail.com>
2023-08-15 16:08:47 -0700

    Update

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A Module.
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Return the JSON rep.
+
+    Args:
+        my_obj: The obj to be converted to a JSON string.
+
+    Returns:
+        str: D JSON repr of obj.
+    """
+    return json.dumps(my_obj)
