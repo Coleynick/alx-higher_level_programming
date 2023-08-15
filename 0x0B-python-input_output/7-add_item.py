f822a38a479f6a44286bd70cf6d3f8c7aa4c3bc4
Coleynick <ezechidinma14@gmail.com>
2023-08-15 16:56:05 -0700

    Script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ A Script.
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
     dat = load_from_json_file("add_item.json")
