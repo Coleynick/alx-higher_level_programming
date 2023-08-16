6a1a7f4b32f63d70c73aa621265efa7ecc38f562
Coleynick <ezechidinma14@gmail.com>
2023-08-15 17:13:48 -0700

    Update

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+A module.
+"""
+
+
+class Student:
+    """
+    A class representing a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dict rep of a Student instance.
+        """
+        if attrs is None:
+            return {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+            }
+        else:
+            attr_dict = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    attr_dict[i] = getattr(self, i)
+            return attr_dict
